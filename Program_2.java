d566499b2c039014ff167dd63878761d29b554e5
abhishekp250397 <72025767+abhishekp250397@users.noreply.github.com>
2020-11-10 08:04:42 +0530

    Add files via upload

---
+++ Program_2.java
@@ -0,0 +1,12 @@
+package Assignment1;
+class Program_2
+{
+    public static void main(String[] args)
+    {
+      int a=74;
+      int b=36;
+      int c= a+b;
+      System.out.println("Addition of 74+36="+c);
+    }
+
+}
